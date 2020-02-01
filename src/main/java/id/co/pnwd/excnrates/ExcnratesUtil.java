f16cb7c78180210d40f3ce2c4c60929222096910
putudoni <putuwigadoni@yahoo.com>
2020-02-01 23:09:38 +0700

    Create get HttpEntity method

--- src/main/java/id/co/pnwd/excnrates/ExcnratesUtil.java
+++ src/main/java/id/co/pnwd/excnrates/ExcnratesUtil.java
@@ -1,5 +1,11 @@
 package id.co.pnwd.excnrates;
 
+import java.util.Arrays;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
 /**
 * Simple exchange rates application with spring boot
 *
@@ -9,4 +15,11 @@ package id.co.pnwd.excnrates;
 */
 public class ExcnratesUtil {
 
+	public static HttpEntity<String> getHttpEntity() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.add("user-agent",AppConstants.USER_AGENT);
+		return new HttpEntity<String>(headers);
+	}
+	
 }
