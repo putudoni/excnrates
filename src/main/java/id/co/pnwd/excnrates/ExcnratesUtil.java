d7e54b9adb67eabdaeb19b0f1f5f69c9d8a5252a
putudoni <putuwigadoni@yahoo.com>
2020-02-01 22:40:48 +0700

    Create utility class

---
+++ src/main/java/id/co/pnwd/excnrates/ExcnratesUtil.java
@@ -0,0 +1,12 @@
+package id.co.pnwd.excnrates;
+
+/**
+* Simple exchange rates application with spring boot
+*
+* @author  Putu Ngurah Wigadoni
+* @version 1.0
+* @since   2020-02-01
+*/
+public class ExcnratesUtil {
+
+}
