085c723b87ca66212b18d49891cc1d658ec9d29d
putudoni <putuwigadoni@yahoo.com>
2020-02-08 13:42:25 +0700

    Modify getRates method

--- src/main/java/id/co/pnwd/excnrates/ExcnRatesService.java
+++ src/main/java/id/co/pnwd/excnrates/ExcnRatesService.java
@@ -3,9 +3,19 @@
  */
 package id.co.pnwd.excnrates;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Putu Ngurah Wigadoni
@@ -15,8 +25,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class ExcnRatesService {
 
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	public List<ResultApi> getRates() {
-		return null;
+		List<ResultApi> result = new ArrayList<>();
+		HttpEntity<String> entity = ExcnratesUtil.getHttpEntity();
+		ResponseEntity<ExchangeRate> rates = null;
+		Countries countries = new Countries();
+		ResultApi resultApi = null;
+		for (Map.Entry<String, String> entry : countries.getCode().entrySet()) {
+			String countryCode = (String) entry.getKey();
+			String currencyCode = (String) entry.getValue();
+			try {
+				rates = restTemplate.exchange(AppConstants.RATESAPI_IO_URL+"/latest?base="+currencyCode+"&symbols=IDR",HttpMethod.GET,entity, ExchangeRate.class);
+				if(rates != null) {
+					BigDecimal rate = rates.getBody().getRates().get(AppConstants.MY_CURRENCY);
+					if(rate !=null) {
+						resultApi = new ResultApi();
+						resultApi.setCountryCode(countryCode);
+						resultApi.setCurrencyCode(currencyCode);
+						resultApi.setRate(rate.setScale(0, BigDecimal.ROUND_HALF_UP));
+						result.add(resultApi);
+					}
+				}
+			} catch (Exception e) {
+				logger.error(e.getMessage());
+			}
+		}
+		return result;
 	}
 	
 }
