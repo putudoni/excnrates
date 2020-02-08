8f569b6acdbd3f59c7d778c95df1653c4873c4c2
putudoni <putuwigadoni@yahoo.com>
2020-02-08 14:04:41 +0700

    Modify index method

--- src/main/java/id/co/pnwd/excnrates/ExcnRatesController.java
+++ src/main/java/id/co/pnwd/excnrates/ExcnRatesController.java
@@ -1,20 +1,11 @@
 package id.co.pnwd.excnrates;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
 
 /**
 * Simple exchange rates application with spring boot
@@ -26,40 +17,12 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class ExcnRatesController {
 	
-	Logger logger = LoggerFactory.getLogger(this.getClass());
-	
-	@Autowired
-	RestTemplate restTemplate;
-	
 	@Autowired
 	private ExcnRatesService excnRatesService;
 	
 	@GetMapping("/rates")
 	public String index(Model model) {
-		HttpEntity<String> entity = ExcnratesUtil.getHttpEntity();
-		ResponseEntity<ExchangeRate> rates = null;
-		List<ResultApi> result = new ArrayList<>();
-		Countries countries = new Countries();
-		ResultApi resultApi = null;
-		for (Map.Entry<String, String> entry : countries.getCode().entrySet()) {
-			String countryCode = (String) entry.getKey();
-			String currencyCode = (String) entry.getValue();
-			try {
-				rates = restTemplate.exchange(AppConstants.RATESAPI_IO_URL+"/latest?base="+currencyCode+"&symbols=IDR",HttpMethod.GET,entity, ExchangeRate.class);
-				if(rates != null) {
-					BigDecimal rate = rates.getBody().getRates().get(AppConstants.MY_CURRENCY);
-					if(rate !=null) {
-						resultApi = new ResultApi();
-						resultApi.setCountryCode(countryCode);
-						resultApi.setCurrencyCode(currencyCode);
-						resultApi.setRate(rate.setScale(0, BigDecimal.ROUND_HALF_UP));
-						result.add(resultApi);
-					}
-				}
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-			}
-		}
+		List<ResultApi> result = excnRatesService.getRates();
 		model.addAttribute("result",result);
 		return "rates";
 	}
