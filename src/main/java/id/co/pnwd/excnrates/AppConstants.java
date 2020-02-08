ca4ed961c474bcd80a170b002a93093502c41c30
putudoni <putuwigadoni@yahoo.com>
2020-02-08 14:02:41 +0700

    Format element

--- src/main/java/id/co/pnwd/excnrates/AppConstants.java
+++ src/main/java/id/co/pnwd/excnrates/AppConstants.java
@@ -10,7 +10,9 @@ package id.co.pnwd.excnrates;
 public class AppConstants {
 
 	public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36";
+	
 	public static final String RATESAPI_IO_URL = "https://api.ratesapi.io/api";
+	
 	public static final String MY_CURRENCY = "IDR";
 	
 }
