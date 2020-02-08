bb8c269bbc8d9252a67a92fdb0f17fc6c546b37c
putudoni <putuwigadoni@yahoo.com>
2020-02-08 13:34:50 +0700

    Create ExcnRatesService class

---
+++ src/main/java/id/co/pnwd/excnrates/ExcnRatesService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package id.co.pnwd.excnrates;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Putu Ngurah Wigadoni
+ * @version 1.0
+ */
+
+@Service
+public class ExcnRatesService {
+
+}
